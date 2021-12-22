b94d22dc705f3a495d5bc25c0ced36d8c9bf2328
Felix S. Klock II <pnkfelix@pnkfx.org>
2021-12-22 10:42:59 -0500

    revise repro script to have a better chance of running in cross-compile setting.

--- repro.sh
+++ repro.sh
@@ -2,10 +2,18 @@
 
 NIGHTLY=nightly-2021-08-22
 
+END_TARGET=aarch64-apple-darwin
+
 rustup update $NIGHTLY
+rustup target add --toolchain $NIGHTLY $END_TARGET
 
-rustc +$NIGHTLY --crate-name rayon_core rayon-core/src/lib.rs --error-format=json --json=diagnostic-rendered-ansi,artifacts --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=42766ff5a1612191 -C extra-filename=-42766ff5a1612191 --out-dir /Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps -L dependency=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps --cap-lints=allow
+# DEPS_DIR=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps
+DEPS_DIR=/tmp/deps
 
-rustc +$NIGHTLY --crate-name a64_doctestfail --edition=2018 src/lib.rs --error-format=json --json=diagnostic-rendered-ansi --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=659d1e8ecf96f9ff -C extra-filename=-659d1e8ecf96f9ff --out-dir /Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps -L dependency=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps --extern rayon_core=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps/librayon_core-42766ff5a1612191.rmeta --cap-lints=allow
+mkdir -p $DEPS_DIR
 
-rustc +$NIGHTLY --crate-type bin --edition 2018 -o /tmp/rust_out -L dependency=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps -L dependency=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps --extern a64_doctestfail=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps/liba64_doctestfail-659d1e8ecf96f9ff.rlib --extern rayon_core=/Users/pnkfelix/Dev/Rust/a64_doctestfail/target/release/deps/librayon_core-42766ff5a1612191.rlib -Ccodegen-units=1 -C lto -Z unstable-options --target aarch64-apple-darwin --color never tests/demo.rs
+rustc +$NIGHTLY --crate-name rayon_core rayon-core/src/lib.rs --error-format=json --json=diagnostic-rendered-ansi,artifacts --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=42766ff5a1612191 -C extra-filename=-42766ff5a1612191 --out-dir $DEPS_DIR -L dependency=$DEPS_DIR --cap-lints=allow --target $END_TARGET
+
+rustc +$NIGHTLY --crate-name a64_doctestfail --edition=2018 src/lib.rs --error-format=json --json=diagnostic-rendered-ansi --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=659d1e8ecf96f9ff -C extra-filename=-659d1e8ecf96f9ff --out-dir $DEPS_DIR -L dependency=$DEPS_DIR --extern rayon_core=$DEPS_DIR/librayon_core-42766ff5a1612191.rmeta --cap-lints=allow --target $END_TARGET
+
+rustc +$NIGHTLY --crate-type bin --edition 2018 -o /tmp/rust_out -L dependency=$DEPS_DIR -L dependency=$DEPS_DIR --extern a64_doctestfail=$DEPS_DIR/liba64_doctestfail-659d1e8ecf96f9ff.rlib --extern rayon_core=$DEPS_DIR/librayon_core-42766ff5a1612191.rlib -Ccodegen-units=1 -C lto -Z unstable-options --target $END_TARGET --color never tests/demo.rs
