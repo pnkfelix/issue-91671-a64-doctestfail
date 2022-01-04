ab6740dc19e3852a273040817ae0f4daa8fe192c
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-01-03 22:35:37 -0500

    further reduction: folded rayon-core into a64_doctestfail crate.

--- repro.sh
+++ repro.sh
@@ -18,16 +18,12 @@ DEPS_DIR=/tmp/deps
 
 mkdir -p $DEPS_DIR
 
-RAYON_EXTRA_FILENAME=-42766ff5a1612191
 A64_D_EXTRA_FILENAME=-659d1e8ecf96f9ff
 
 set -e
 
-echo "BUILD RAYON_CORE"
-$RUSTC --crate-name rayon_core rayon-core/src/lib.rs --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=42766ff5a1612191 -C extra-filename=$RAYON_EXTRA_FILENAME --out-dir $DEPS_DIR -L dependency=$DEPS_DIR --cap-lints=allow --target $END_TARGET
-
 echo "BUILD A64_DOCTESTFAIL"
-$RUSTC --crate-name a64_doctestfail --edition=2018 src/lib.rs --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=659d1e8ecf96f9ff -C extra-filename=$A64_D_EXTRA_FILENAME --out-dir $DEPS_DIR -L dependency=$DEPS_DIR --extern rayon_core=$DEPS_DIR/librayon_core$RAYON_EXTRA_FILENAME.rmeta --cap-lints=allow --target $END_TARGET
+$RUSTC --crate-name a64_doctestfail --edition=2018 src/lib.rs --crate-type lib --emit=dep-info,metadata,link -C opt-level=3 -C linker-plugin-lto -C metadata=659d1e8ecf96f9ff -C extra-filename=$A64_D_EXTRA_FILENAME --out-dir $DEPS_DIR -L dependency=$DEPS_DIR --cap-lints=allow --target $END_TARGET
 
 echo "BUILD DEMO"
 
@@ -39,4 +35,4 @@ echo "BUILD DEMO"
 # -Z print-llvm-passes=yes
 # -C llvm-args=-debug-only=instruction-select,aarch64-isel
 
-$RUSTC --crate-type bin --edition 2018 -o /tmp/rust_out$REPRO_SUFFIX -L dependency=$DEPS_DIR -L dependency=$DEPS_DIR --extern a64_doctestfail=$DEPS_DIR/liba64_doctestfail$A64_D_EXTRA_FILENAME.rlib --extern rayon_core=$DEPS_DIR/librayon_core$RAYON_EXTRA_FILENAME.rlib -Ccodegen-units=1 -C lto -Z unstable-options --target $END_TARGET --color never tests/demo.rs
+$RUSTC --crate-type bin --edition 2018 -o /tmp/rust_out$REPRO_SUFFIX -L dependency=$DEPS_DIR -L dependency=$DEPS_DIR --extern a64_doctestfail=$DEPS_DIR/liba64_doctestfail$A64_D_EXTRA_FILENAME.rlib -Ccodegen-units=1 -C lto -Z unstable-options --target $END_TARGET --color never tests/demo.rs
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,100 @@
+#![feature(lang_items)]
+#![feature(fundamental)]
+#![feature(auto_traits)]
 #![feature(no_core)]
 #![no_core]
 
-// lib.rs
+mod core {
+    pub mod deref {
+        use crate::Sized;
 
-extern crate rayon_core;
+        #[lang = "receiver"]
+        pub trait Receiver { }
 
-/// ```
-/// use a64_doctestfail;
-/// ```
+        impl<T: ?Sized> Receiver for &T {}
+        impl<T: ?Sized> Receiver for &mut T {}
+    }
+
+    pub mod marker {
+        #[lang = "freeze"]
+        pub(crate) unsafe auto trait Freeze {}
+
+        #[lang = "copy"]
+        pub trait Copy { }
+
+        #[lang = "sized"]
+        #[fundamental]
+        pub trait Sized { }
+    }
+
+    pub mod result {
+        pub enum Result<T, E> {
+            #[lang = "Ok"]
+            Ok(T),
+
+            #[lang = "Err"]
+            Err(E),
+        }
+
+        impl<T, E> Result<T, E> {
+            pub fn map_err(self, op: fn(E) -> &'static E) -> Result<T, &'static E> {
+                loop { }
+            }
+        }
+    }
+}
+
+use core::marker::Sized;
+use core::result::Result;
+
+mod std {
+    #[lang = "eh_personality"]
+    extern "C" fn eh_personality() {}
+
+    pub(crate) mod io {
+        pub enum Error {
+            Os(i32),
+            SimpleMessage(usize),
+        }
+    }
+}
+
+fn registry_new(mut builder: impl Sized) -> Result<(), BuildError> { loop { } }
+
+mod tp {
+    use crate::{BuildError};
+    use crate::core::result::Result;
+
+    pub struct Pool { }
+
+    pub(super) fn thread_pool_build() -> Result<(), BuildError>
+    {
+        let val = crate::registry_new(());
+        let registry = match val {
+            Result::Ok(x) => x,
+            Result::Err(y) => return Result::Err(crate::id(y)),
+        };
+        loop { }
+    }
+
+    impl Pool {
+        pub fn new() {
+            let r: Result<_, &'static BuildError> = thread_pool_build().map_err(crate::alloc_ref);
+            loop { }
+        }
+    }
+}
+
+fn alloc_ref<T>(x: T) -> &'static T { loop { } }
+
+fn id<T>(x: T) -> T { x }
+
+pub struct BuildError {
+    kind: std::io::Error,
+}
+
+pub trait Trait { }
+
+pub fn build<'a>(_: &'a ()) -> Result<tp::Pool, &'a dyn Trait> { loop { } }
+
 pub struct X;
